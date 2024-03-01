f4a167f1df19bd5515b82cf40bf76b45a2ab5315
VictorMaterco <vmaterco@gmail.com>
2024-03-01 18:31:07 -0300

    correcao bug

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const form = document.getElementById('form-atividade');
 const imgAprovado = `<img src="./images/aprovado.png" alt="Emoji Festejando" />`
 const imgReprovado = `<img src="./images/reprovado.png" alt="Emoji Decepcionado" />`
 const atividades = [];
-const notas =  [];
+const notas = [];
 const spanAprovado = `<span class="resultado aprovado">Aprovado</span>`;
 const spanReprovado = `<span class="resultado reprovado">Reprovado</span>`;
 const notaMinima = parseFloat(prompt('Digite a nota mínima:'));
@@ -18,15 +18,15 @@ form.addEventListener('submit', function(e) {
     atualizaMediaFinal();
 });
 
-function adicionaLinha(){
+function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    if(atividades.includes(inputNomeAtividade.value)){
+    if (atividades.includes(inputNomeAtividade.value)) {
         alert(`A atividade: ${inputNomeAtividade.value} já foi inserida`);
     } else {
         atividades.push(inputNomeAtividade.value);
-        notas.push(parseFloat(inputNotaAtividade));
+        notas.push(parseFloat(inputNotaAtividade.value));
         
         let linha = '<tr>';
         linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -47,7 +47,7 @@ function atualizaTabela() {
     corpoTabela.innerHTML = linhas;
 }
 
-function atualizaMediaFinal(){
+function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
